ce0c556510e34e08201e53da84ed3e8081836f99
Mordecai Yang <FankYung@gmail.com>
2020-08-09 12:21:52 +0800

    init js

---
+++ dataService.js
@@ -0,0 +1,11 @@
+window.dataService = new ( function() {
+	let _self = this;
+	
+	_self.init = function () {
+		console.log('init dataService');
+		
+	} 
+
+	
+	_self.init();
+})();
---
+++ mainComponent.js
@@ -0,0 +1,11 @@
+window.mainComponent = new ( function() {
+	let _self = this;
+	
+	_self.init = function () {
+		console.log('init mainComponent');
+		
+	} 
+
+	
+	_self.init();
+})();
